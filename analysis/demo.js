4a36c0f553b2ea474a6ecee1afb67e127d23a124
Loeyyi <fionaliao0315@gmail.com>
2023-01-08 00:59:55 +0800

    Update demo.js

--- analysis/demo.js
+++ analysis/demo.js
@@ -1,30 +1,28 @@
-var menuOpen = false; 
+var menuOpen = false;
 
-function showMenuItems (event){
+function showHideMenuItems (event) {
 	event.stopPropagation();
 	let menuItems = $('nav > a:not(:first-of-type)');
 	if(menuOpen) {
-		//menuItems.css('display', 'none');
 		menuItems.removeClass('open');
-		$('nav > a >span').removeClass('rotated');
+		$('nav > a > span').removeClass('rotated');
 		menuOpen = false;
 	}
 	else {
-		//menuItems.css('display', 'inline');
-		$('nav > a >span').addClass('rotated');
+		menuItems.addClass('open');
+		$('nav > a > span').addClass('rotated');
 		menuOpen = true;
 	}
 }
-function hideMenuItems (){
+
+function hideMenuItems () {
 	let menuItems = $('nav > a:not(:first-of-type)');
-	//menuItems.css('display', 'none');
 	menuItems.removeClass('open');
-	$('nav > a >span').removeClass('rotated');
+	$('nav > a > span').removeClass('rotated');
 	menuOpen = false;
 }
 
-window.onload = function (){
-	$('nav > a:first-of-type').on('click', showMenuItems);
+window.onload = function () {
+	$('nav > a:first-of-type').on('click', showHideMenuItems);
 	$(document).on('click', hideMenuItems);
 };
-
